cd13e986577c1e34f47c75d64ba1cb9eef62ad99
Kapil Dev S <kapilsekar@gmail.com>
2018-11-17 09:48:17 +0530

    JPA implementation

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,14 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
+
 import tacos.Order;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 
-	Order save(Order order);
+//	Order save(Order order);
+	
+//	List<Order> findByDeliveryZip(String deliveryZip);
+//	
+//	List<Order> readOrderByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 }
